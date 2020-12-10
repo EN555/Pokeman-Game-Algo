cd7a5fa6c53cadeded530d087e27fee73533ff6b
nir_son <nir16832@gmail.com>
2020-12-10 11:56:37 +0200

    g

--- gameClient/gameClient/MyFrame.java
+++ gameClient/gameClient/MyFrame.java
@@ -1,7 +1,9 @@
 package gameClient;
 
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
+import java.awt.event.ComponentListener;
 
 import javax.swing.JFrame;
 
@@ -23,6 +25,7 @@ public class MyFrame extends JFrame{
 		this.setSize((int)(screenSize.width/2), (int)(screenSize.height/2));		//the size of the frame will be half of the screen
 		this.setLocation((int)(screenSize.width/4), (int)(screenSize.height/4));		//update it to the middle of the screen
 
+		
 		MyPanel panel = new MyPanel(this.getWidth(), this.getHeight() , arena);
 		this.add(panel);
 	}
@@ -33,10 +36,13 @@ public class MyFrame extends JFrame{
 
 	public static void main(String [] args) {
 
-		game_service game = Game_Server_Ex2.getServer(3);
+		game_service game = Game_Server_Ex2.getServer(12);
 		game.addAgent(0);
 		Arena arena = new Arena(game.getGraph() , game.getPokemons() , game.getAgents());
-		MyFrame g = new MyFrame("level 3" , arena);
+		MyFrame g = new MyFrame("level 12" , arena);
+		
+		//g.repaint();
+		
 		
 		//System.out.println(game.getGraph());
 		
@@ -45,7 +51,6 @@ public class MyFrame extends JFrame{
 //		}
 		
 		g.setVisible(true);
-		//g.repaint();
 	}
 
 
--- test/ex2/test1.java
+++ test/ex2/test1.java
@@ -1,18 +1,42 @@
 package ex2;
 
 import Server.Game_Server_Ex2;
+import api.DWGraph_Algo;
 import api.game_service;
 
 public class test1 {
 	public static void main(String[] arg) {
 	
-		game_service game = Game_Server_Ex2.getServer(2);
+		game_service game = Game_Server_Ex2.getServer(20);
 		
-		game.addAgent(0);
+		//
 		
-		String s = game.getPokemons();
+		DWGraph_Algo ga = new DWGraph_Algo();
 		
-		System.out.println(game);
+		ga.load("data/A5");
+		
+		System.out.println(ga.isConnected());
+		
+		
+//		int[] arr = {1,2,3};
+//		
+//		int num_of_agents = 3;
+//
+//		for(int i=0 ; i<num_of_agents ; i++) {
+//			game.addAgent(arr[i]);
+//		}
+//		System.out.println(game.getAgents());
+//		
+//		game.chooseNextEdge(0, 0);
+//		System.out.println(game.getAgents());
+		
+		
+		
+//		game.addAgent(0);
+//		
+//		String s = game.getPokemons();
+//		
+//		System.out.println(game);
 		
 		
 	}
